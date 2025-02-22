cf6da918f9569cb75584d17cc14762e9c9c76c05
Thiago099 <tecnologiasthiago@gmail.com>
2025-02-22 13:45:02 -0300

    fixed github pages config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  //base: '/Texture-Composer/', // This setting is for github pages only
   build: {
-    //base: '/Texture-Composer/', // This setting is for web only
     outDir: './TextureComposer\\TextureComposer\\public', // This setting is for web view only
-    emptyOutDir: true, // also necessary
+    emptyOutDir: true,
   }
 })
