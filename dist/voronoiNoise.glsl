13db8522cd48e6c04b7890bc12d618baf4768257
Thiago099 <tecnologiasthiago@gmail.com>
2025-02-23 12:48:50 -0300

    Deploy to GitHub Pages

---
+++ dist/voronoiNoise.glsl
@@ -0,0 +1,39 @@
+//	<https://www.shadertoy.com/view/Xd23Dh>
+//	by inigo quilez <http://iquilezles.org/www/articles/voronoise/voronoise.htm>
+
+#Fragment Shader
+
+precision mediump float;
+
+vec3 hash3( vec2 p )
+{
+    vec3 q = vec3( dot(p,vec2(127.1,311.7)), 
+				   dot(p,vec2(269.5,183.3)), 
+				   dot(p,vec2(419.2,371.9)) );
+	return fract(sin(q)*43758.5453);
+}
+
+float voronoise( in vec2 p, float u, float v )
+{
+	float k = 1.0+63.0*pow(1.0-v,6.0);
+
+    vec2 i = floor(p);
+    vec2 f = fract(p);
+    
+	vec2 a = vec2(0.0,0.0);
+    for( int y=-2; y<=2; y++ )
+    for( int x=-2; x<=2; x++ )
+    {
+        vec2  g = vec2( x, y );
+		vec3  o = hash3( i + g )*vec3(u,u,1.0);
+		vec2  d = g - f + o.xy;
+		float w = pow( 1.0-smoothstep(0.0,1.414,length(d)), k );
+		a += vec2(o.z*w,w);
+    }
+	
+    return a.x/a.y;
+}
+
+float voronoi(vec2 uv, float disturbance, float contrast, float scale){
+    return voronoise(uv * scale * 8.0, disturbance, contrast); 
+}
